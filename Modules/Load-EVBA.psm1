d23175859ecce373e4ed1881689c35aee5809e02
isisgomez28 <igomezp.28@gmail.com>
2014-04-25 13:35:29 -0400

    SomeDocs
    Documentos entregables para el proyecto

---
+++ Modules/Load-EVBA.psm1
@@ -0,0 +1,26 @@
+﻿
+    $excel = new-object -comobject Excel.Application
+    $excel.Visible = $true
+    $Workbook = $excel.workbooks.Add()
+    $Worksheets = $excel.Worksheets
+
+    function Sheets($id) {
+        return $Worksheets.Item($id)
+    }
+
+    function Workbooks {
+        return $excel.Worksheet
+    }
+
+    function Range() {
+        return $excel.Range
+    }
+
+Export-ModuleMember -Function Load-EVBA
+Export-ModuleMember -Function Sheets
+Export-ModuleMember -Function Workbooks
+Export-ModuleMember -Function Range
+Export-ModuleMember -Variable $excel
+Export-ModuleMember -Variable $Workbook
+Export-ModuleMember -Variable $Worksheets
+
